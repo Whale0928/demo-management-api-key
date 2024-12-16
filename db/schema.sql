99c466f6a3e0fa91a31abf72d547816833149a29
hgkim <hgkim@openrd.com>
2024-12-16 23:20:35 +0900

    docs: update README.md

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,10 @@ CREATE SEQUENCE clients_id_seq;
 create table clients
 (
     id          integer      not null default nextval('clients_id_seq'),
-    name        varchar(255) not null,        -- 키 이름
-    email       varchar(255) not null unique, -- 이메일
-    api_key     varchar(512) not null unique, -- api key
-    issuer_info varchar(255) not null,        --  발급자 정보
+    name        varchar(255) not null,
+    email       varchar(255) not null unique,
+    api_key     varchar(512) not null unique,
+    issuer_info varchar(255) not null,
     permissions text array,
     allowed_ips text array,
     issued_at   timestamp,
